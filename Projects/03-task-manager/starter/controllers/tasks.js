36321996611b06f903789a1721f4ccaf823a0650
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-20 02:25:50 +0530

    get single task, create task completed

--- Projects/03-task-manager/starter/controllers/tasks.js
+++ Projects/03-task-manager/starter/controllers/tasks.js
@@ -1,15 +1,33 @@
 const Task = require("../models/Task");
-const getAllTasks = (req, res) => {
-  res.send("Get All Tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.send("Get Single Task");
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` }); // not getting same number of characters as id that is needed by mongoose.
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(404).json({ msg: error }); // when there is more than the number of characters added in id.(cast error)
+  }
 };
 const updateTask = (req, res) => {
   res.send("Update Task");
