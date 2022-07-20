d61b603ee69bce51ba9f2fd3fb688f9016dbf88f
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-21 01:15:05 +0530

    updateTask controller

--- Projects/03-task-manager/starter/controllers/tasks.js
+++ Projects/03-task-manager/starter/controllers/tasks.js
@@ -29,8 +29,20 @@ const getTask = async (req, res) => {
     res.status(500).json({ msg: error }); // when there is more than the number of characters added in id.(cast error) or syntax error in mentioning id.
   }
 };
-const updateTask = (req, res) => {
-  res.send("Update Task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true, // to get the new value
+      runValidators: true, // to run the validators, else it won't validate.
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const deleteTask = async (req, res) => {
   try {
