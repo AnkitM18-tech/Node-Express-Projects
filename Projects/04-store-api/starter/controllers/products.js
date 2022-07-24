581fc342c7f28f4dd6232a71fd712d8f3ae354f5
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-24 18:11:07 +0530

    testing the async-error package

--- Projects/04-store-api/starter/controllers/products.js
+++ Projects/04-store-api/starter/controllers/products.js
@@ -1,4 +1,5 @@
 const getAllProductsStatic = async (req, res) => {
+  throw new Error("Testing Async Package"); // instead of setting try..catch (own middleware) we can use express-async-error, which will handle the errors.No need to use async wrapper here.
   res.status(200).json({ msg: "Product Testing Route" });
 };
 const getAllProducts = async (req, res) => {
