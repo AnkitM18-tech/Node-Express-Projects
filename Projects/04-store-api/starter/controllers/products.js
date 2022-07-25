29263300214961bb2b4da2e755321aa900c18d7f
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-26 03:43:25 +0530

    adding limit,pages and skips to products

--- Projects/04-store-api/starter/controllers/products.js
+++ Projects/04-store-api/starter/controllers/products.js
@@ -3,7 +3,11 @@ const Product = require("../models/product");
 // for manually testing purpose
 const getAllProductsStatic = async (req, res) => {
   // const search = "a";
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10)
+    .skip(5);
   // const products = await Product.find({}).sort("-name price");
   // name: { $regex: search, $options: "i" }, // regex query operator
   /* throw new Error("Testing Async Package"); // instead of setting try..catch (own middleware) we can use express-async-error, which will handle the errors.No need to use async wrapper here. */
@@ -38,6 +42,13 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+  // adding limit,pagination and skips
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  // 23 -> 7 7 7 2 -> 4 pages
+  result = result.skip(skip).limit(limit);
+  // before we await for the result we need to chain the params.
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
