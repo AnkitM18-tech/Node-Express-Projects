001cda15336505b71702ba769b13e9b43e62e9a2
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-27 03:10:03 +0530

    jwt token

--- Projects/05-JWT-Basics/starter/controllers/main.js
+++ Projects/05-JWT-Basics/starter/controllers/main.js
@@ -1,5 +1,23 @@
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
-  res.send("Fake Registration Route");
+  // mongoose validations
+  // JOI
+  // Check in controlller -> async wrapper , next handler -> Task Manager
+  const { username, password } = req.body;
+  // console.log(username, password);
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide username and password", 400);
+  }
+  // Demo
+  const id = new Date().getDate();
+  // try to keep payload small, better experience for user -- in production give complex unguesssable secret keys.- because it is used to sign our tokens.so if anyone gets a hold of this key then they can sign tokens instead of us.--read more about jwt on jwt.io.
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "User Created", token });
 };
 
 const dashboard = async (req, res) => {
@@ -14,3 +32,9 @@ module.exports = {
   login,
   dashboard,
 };
+
+// HTTP requests is stateless, server doesn't know / doesn't remember any previous requests sent by browser. So everytime frontend will still need to provide a valid token., else access will be denied.
+
+// response + Signed JWT
+// browser <----------- server ==> Look for JWT.io to know more about JWT.
+// JWT contains => Header,Payload,Signature... can be verified.
