22251c920616d63a1bfc03849ca310dd801e1fc7
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-29 03:46:11 +0530

    created jwtToken in controller

--- Projects/06-jobs-api/starter/controllers/auth.js
+++ Projects/06-jobs-api/starter/controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   /* const { name, email, password } = req.body;
@@ -14,7 +15,10 @@ const register = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, salt);
   const tempUser = { name, email, password: hashedPassword }; */
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
