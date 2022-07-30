d6389e1ef8d6eec7ad94d1050ebb27894f3988c0
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-30 20:37:54 +0530

    get all jobs method

--- Projects/06-jobs-api/starter/controllers/jobs.js
+++ Projects/06-jobs-api/starter/controllers/jobs.js
@@ -2,7 +2,8 @@ const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 const getAllJobs = async (req, res) => {
-  res.send("Get All Jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 const getJob = async (req, res) => {
   res.send("Get Job");
